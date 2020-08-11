1c35e8cd8a025be6f708e7f9cd999625e1049577
David Reke <david.e.reke@gmail.com>
2020-08-11 15:22:51 -0400

    Updates

---
+++ precache-manifest.e5b611522dd3ae4705578bf819e6c877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3f85e18d0399a35a52b7477978e304",
+    "url": "/chuckNorrisAPIExample/index.html"
+  },
+  {
+    "revision": "4d1da3a911400a064305",
+    "url": "/chuckNorrisAPIExample/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36ddf0e7f033ca2250c3",
+    "url": "/chuckNorrisAPIExample/static/js/2.9f7e5897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuckNorrisAPIExample/static/js/2.9f7e5897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1da3a911400a064305",
+    "url": "/chuckNorrisAPIExample/static/js/main.5199d308.chunk.js"
+  },
+  {
+    "revision": "0ed4217924a3b386c3b7",
+    "url": "/chuckNorrisAPIExample/static/js/runtime-main.34a425d1.js"
+  }
+]);
